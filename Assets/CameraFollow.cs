f28b9f4bfc272a91892bc64b6c91f371fe138325
femisd <femisd@gmail.com>
2020-05-25 00:52:08 +0100

    added movable obstacles

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+
+    private void LateUpdate()
+    {
+
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothedPosition = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, targetPosition.y, smoothSpeed * Time.deltaTime), transform.position.z);
+        transform.position = smoothedPosition;
+
+    }
+}
--- Assets/EndTrigger.cs
+++ Assets/EndTrigger.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 public class EndTrigger : MonoBehaviour
 {
     public GameObject completeLevelUI;
+    public GameObject package;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        CompleteLevel();
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            CompleteLevel();
+        }
+        Rigidbody2D packageBody = collision.gameObject.GetComponent<Rigidbody2D>();
+        packageBody.velocity = Vector2.zero;
+        packageBody.freezeRotation = true;
     }
 
 
