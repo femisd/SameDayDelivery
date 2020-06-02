84d2d7dea820d4a4c0b295a4cb4ed1d98964a270
femisd <femisd@gmail.com>
2020-06-03 00:41:03 +0100

    added main menu

---
+++ Assets/SelectLevel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour
+{
+    // Really dookie solution but this makes WAY easier to asign functionality to the onclick listener of buttons in unity
+    // TODO: find a better solution (when I'm less baked...)
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Map1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Map2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Map3");
+    }
+
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene("Map4");
+    }
+
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene("Map5");
+    }
+
+    public void LoadLevel6()
+    {
+        SceneManager.LoadScene("Map6");
+    }
+
+    public void LoadLevel7()
+    {
+        SceneManager.LoadScene("Map7");
+    }
+
+}
