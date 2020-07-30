5f40450f9ff5251bea995cff5c8e15b033435005
femisd <femisd@gmail.com>
2020-07-30 14:12:19 +0100

    initial post processing setup

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Vector3 = UnityEngine.Vector3;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
+    private Transform target;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
 
+    private void Update()
+    {
+        target = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+    }
     private void LateUpdate()
     {
 
