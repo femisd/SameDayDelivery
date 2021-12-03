1c13c565419af5c9fe256dfcf2afd18dc61a41f7
femisd <femisd@gmail.com>
2021-12-03 15:37:10 +0000

    Refactoring
    Project clean up

---
+++ Assets/Scripts/Utils/SelectLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour
+{
+
+
+    public void selectLevelByName(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+}
